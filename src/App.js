3fd5112bb3c7a929a3d602d8f5fde977acff03d7
Sailaja Nandela <snandyala0@gmail.com>
2018-10-03 02:31:39 -0500

    final commit

---
+++ src/App.js
@@ -0,0 +1,71 @@
+import React from 'react'
+import * as BooksAPI from './BooksAPI'
+import './App.css'
+import { Route, Link } from 'react-router-dom'
+import Search from './components/Search';
+import BookCase from './components/BookCase'
+
+class App extends React.Component {
+  state = {
+      books: []
+  };
+
+  //will get our initial state of the page by getting all books
+  //then filtering them out by their respective states
+  componentDidMount() {
+    BooksAPI.getAll().then((books) => {
+      this.setState({
+          books: books
+      });
+    })
+  }
+
+  moveBookShelf = (book, newValue) => {
+
+      book.props.book.shelf = newValue;
+
+      this.setState( (state) => ({
+          books: state.books.filter( (b) => b.id !== book.props.book.id).concat([book.props.book])
+      }))
+
+      BooksAPI.update(book.props.book, newValue);
+
+  }
+
+
+  render() {
+    return (
+      <div className="app">
+        <Route exact path="/" render={() => (
+            <div className="list-books">
+              <div className="list-books-title">
+                <h1>MyReads</h1>
+              </div>
+              <BookCase
+                books={this.state.books}
+                onBookShelfChange={this.moveBookShelf}
+              />
+              <div className="open-search">
+                <Link
+                  to="/search"
+                >Add a book</Link>
+              </div>
+            </div>
+        )}
+        />
+
+        <Route path="/search" render={() => (
+            <Search
+                bsBooks={this.state.books}
+                onBookShelfChange={this.moveBookShelf}
+            />
+        )}
+
+        />
+
+      </div>
+    )
+  }
+}
+
+export default App
